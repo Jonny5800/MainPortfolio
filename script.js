d534108e73008cbc627917d4ecd243deb251b244
Jonny <jonathan.hall5800@gmail.com>
2023-06-04 15:32:08 +0100

    checking mobile view

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+const imageElement = document.querySelector(".lightbulb");
+
+function pulseImage() {
+  imageElement.style.transition = "transform 0.2s";
+  imageElement.style.transform = "scale(0.6)";
+  setTimeout(() => {
+    imageElement.style.transition = "transform 0.3s";
+    imageElement.style.transform = "scale(1.3)";
+    setTimeout(() => {
+      imageElement.style.transition = "transform 0.2s";
+      imageElement.style.transform = "scale(1)";
+    }, 800);
+  }, 5200);
+}
+
+setInterval(pulseImage, 1200);
+
+setInterval(pulseImage, 2000);
+
 document.querySelector(".send-button").addEventListener("click", function () {
   var name = document.querySelector(".contact-input-name").value;
   var email = "jonathan.hall5800@gmail.com";
   var message = document.querySelector(".contact-input-message").value;
-  //   var userEmail = document.querySelector(".contact-input-email").value;
 
   var subject = encodeURIComponent("Hi I'm " + name + ", I saw your portfolio");
   var body = encodeURIComponent(
